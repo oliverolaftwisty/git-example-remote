0e134f062569c44ff671f7ef06a428e8125b7589
oliverolaftwisty <211250214@smail.nju.edu.cn>
2021-10-14 19:20:49 +0800

    i am a performance artist

---
+++ cmake-build-debug/CMakeFiles/git_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_example.dir/main.c.o"
+  "git_example"
+  "git_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
